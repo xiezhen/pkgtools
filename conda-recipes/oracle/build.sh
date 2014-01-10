43ee950866a8bad5880f02af3e6071e0b133e402
Zhen Xie <zhen@pczhen.cern.ch>
2014-01-10 20:05:01 +0100

    add dependency

--- conda-recipes/oracle/build.sh
+++ conda-recipes/oracle/build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-
-cp --preserve=links $SRC_DIR/bin/sqlplus $PREFIX/bin
+mkdir -p  $PREFIX/bin
+mkdir -p  $PREFIX/lib
+cp $SRC_DIR/bin/sqlplus $PREFIX/bin
 find  $SRC_DIR/lib/ -name "libclntsh.so*" -print | while read filename
 do
-  cp --preserve=links "${filename}" $PREFIX/lib 
+  cp -P --preserve=links "${filename}" $PREFIX/lib 
 done
-cp --preserve=links $SRC_DIR/lib/libsqlplus.so $PREFIX/lib
-cp --preserve=links $SRC_DIR/lib/libnnz11.so $PREFIX/lib
-
-#cp -r --preserve=links $SRC_DIR/include/. $PREFIX/include/oracle
+cp -P $SRC_DIR/lib/libsqlplus.so $PREFIX/lib
+cp -P $SRC_DIR/lib/libnnz11.so $PREFIX/lib
 
 # See
 # http://docs.continuum.io/conda/build.html
