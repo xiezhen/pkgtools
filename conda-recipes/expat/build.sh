bf747d7d7b940af5424686a95b2952d76a29bb95
Zhen Xie <zhen@pczhen.cern.ch>
2014-02-24 09:43:20 +0100

    removed

--- conda-recipes/expat/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp -r --preserve=links $SRC_DIR/lib/. $PREFIX/lib 
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
