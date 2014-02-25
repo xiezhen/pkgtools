c385adb8bcdfe2a714745819d3a62a532c919f88
Zhen Xie <zhen@pczhen.cern.ch>
2014-02-25 19:06:49 +0100

    increase receipe tag number

--- buildlumipkgs.sh
+++ buildlumipkgs.sh
@@ -9,12 +9,12 @@ cd ${CONDADIR}
 source ${CONDADIR}/bin/activate ${CONDADIR}
 
 LUMICONDA_RECEIPE="https://github.com/xiezhen/lumiconda-receipes.git"
-LUMICONDA_RECEIPE_TAG="v0.0.2"
+LUMICONDA_RECEIPE_TAG="v0.0.3"
 git clone ${LUMICONDA_RECEIPE}
 git checkout $LUMICONDA_RECEIPE_TAG
 cd "lumiconda-receipes"
 
-PKG_LIST=('xz' 'expat' 'libstdcplus' 'pacparser' 'xerces-c' 'boost' 'frontier_client' 'oraclesdk' 'oracleclientlite' 'sqlplus' 'coral' 'root')
+PKG_LIST=('xz' 'expat' 'libstdcplus' 'pacparser' 'xerces-c' 'boost' 'frontier_client' 'oraclesdk' 'oracleclientlite' 'sqlplus' 'coral' 'libx11' 'libxpm' 'libxext' 'libxft' 'root')
 for pkgname in "${PKG_LIST[@]}"
 do
     conda build $pkgname
